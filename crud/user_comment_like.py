5f3407ce8ae0ca56a12001e8a11a652340dbbfeb
SeungHyeon <namsh1125@naver.com>
2023-12-16 17:22:23 +0900

    [Feat] user_comment_like.py 구현

--- crud/user_comment_like.py
+++ crud/user_comment_like.py
@@ -1,36 +1,63 @@
 from sqlalchemy.orm import Session
+from models import Comment, UserCommentLike
+from schemas.comment_like import CommentLikeDto
 
-import models
 
+def toggle_like(db: Session, blog_id: str, comment_id: str) -> CommentLikeDto:
+    # 댓글 좋아요 정보 조회
+    user_comment_like = (db.query(UserCommentLike)
+                         .filter(UserCommentLike.blog.blog_id == blog_id,
+                                 UserCommentLike.comment.comment_id == comment_id).first())
 
-def create_user_comment_like(db: Session, user_comment_like_data: dict):
-    db_user_comment_like = models.UserCommentLike(**user_comment_like_data)
-    db.add(db_user_comment_like)
+    if user_comment_like:
+        # 댓글 좋아요 했던 상태면 좋아요 취소
+        db.delete(user_comment_like)
+        decrease_comment_likes_count(db, comment_id)  # 좋아요 취소 시 개수 감소
+        is_liked = False
+
+    else:
+        # 댓글 좋아요 하지 않았던 상태면 좋아요 추가
+        new_like = UserCommentLike(blog_id=blog_id, comment_id=comment_id)
+        db.add(new_like)
+        increase_comment_likes_count(db, comment_id)  # 좋아요 추가 시 개수 증가
+        is_liked = True
+
     db.commit()
-    db.refresh(db_user_comment_like)
-    return db_user_comment_like
+
+    # 결과 리턴
+    return CommentLikeDto(blog_id=blog_id, comment_id=comment_id, is_liked=is_liked)
 
 
-def get_user_comment_like(db: Session, user_comment_like_id: int):
-    return db.query(models.UserCommentLike).filter(models.UserCommentLike.id == user_comment_like_id).first()
+def increase_comment_likes_count(db: Session, comment_id: str):
+    try:
+        # 댓글 정보 조회
+        comment = db.query(Comment).filter(Comment.comment_id == comment_id).first()
+
+        if comment:
+            # 댓글 좋아요 개수 증가
+            comment.likes_count += 1
+
+            # DB에 반영
+            db.commit()
+
+    except Exception as e:
+        # 예외 처리 로직 추가
+        raise e
 
 
-def get_user_comment_likes(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.UserCommentLike).offset(skip).limit(limit).all()
+def decrease_comment_likes_count(db: Session, comment_id: str):
+    try:
+        # 댓글 정보 조회
+        comment = db.query(Comment).filter(Comment.comment_id == comment_id).first()
 
+        if comment:
+            # 댓글 좋아요 개수 줄이기
+            if comment.likes_count > 0:
+                comment.likes_count -= 1
 
-def update_user_comment_like(db: Session, user_comment_like_id: int, user_comment_like_data: dict):
-    db_user_comment_like = db.query(models.UserCommentLike).filter(
-        models.UserCommentLike.id == user_comment_like_id).first()
-    for key, value in user_comment_like_data.items():
-        setattr(db_user_comment_like, key, value)
-    db.commit()
-    db.refresh(db_user_comment_like)
-    return db_user_comment_like
+            # DB에 반영
+            db.commit()
 
-
-def delete_user_comment_like(db: Session, user_comment_like_id: int):
-    db_user_comment_like = db.query(models.UserCommentLike).filter(
-        models.UserCommentLike.id == user_comment_like_id).first()
-    db.delete(db_user_comment_like)
-    db.commit()
+    except Exception as e:
+        # 예외 처리 로직 추가
+        raise e
